2404a06342dee8a8e3e23fd7f1efe51082ee42ca
Raees-Khan6 <raees.khan@city.ac.uk>
2026-03-03 13:03:38 +0000

    Athlete API GET fix
    - Fix to support ruleset
    - Fix to return top 10 from either no gi or gi top 10 and not just gi
    - Ensures althetes are filtered with an ELO > 0

--- app/api/athletes/route.ts
+++ app/api/athletes/route.ts
@@ -3,10 +3,19 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
+export async function GET(request: Request) {
     try{
+        const { searchParams } = new URL(request.url);
+        const ruleset = searchParams.get('ruleset') || 'gi';
+        const order = ruleset == 'nogi' ? 'noGiEloRating' : 'giEloRating';
         const athletes = await prisma.athlete.findMany({
-            orderBy:{giEloRating: 'desc'}, take: 10 
+            where: {
+                [order]: { gt: 0}
+            },
+            orderBy: {
+                [order]: 'desc'
+            },
+            take: 10
         });
 
 
